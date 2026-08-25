686b194dd17308dc2b95342a8a18276d4e63737d
NidhinGJoseph <nidhinjoseph1331@gmail.com>
2026-08-25 18:07:43 +0530

    Configure production PostgreSQL database

--- navapackbackend/settings.py
+++ navapackbackend/settings.py
@@ -1,3 +1,5 @@
+import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,12 +53,23 @@ TEMPLATES = [
 WSGI_APPLICATION = "navapackbackend.wsgi.application"
 ASGI_APPLICATION = "navapackbackend.asgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = "en-us"
