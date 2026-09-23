22da24ea4011ce54315f962f22f7ca92d151b36d
anjana51 <anjanaanilk781@gmail.com>
2026-09-23 17:49:06 +0530

    marketing

--- Auth/views.py
+++ Auth/views.py
@@ -3,8 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from .serializers import LoginSerializer
 from .serializers import SignupSerializer
+from .serializers import UserListSerializer
 
 class LoginAPIView(APIView):
     """
@@ -27,6 +29,7 @@ class LoginAPIView(APIView):
                 'email': user.email,
                 'first_name': user.first_name,
                 'last_name': user.last_name,
+                'department': user.profile.department,
             }
         }, status=status.HTTP_200_OK)
 
@@ -51,6 +54,16 @@ class SignupAPIView(APIView):
                 'is_approved': user.profile.is_approved,
             }
         }, status=status.HTTP_201_CREATED)
+
+
+class UserListAPIView(APIView):
+    """List all users with profile details."""
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        users = User.objects.select_related('profile').all().order_by('-date_joined')
+        serializer = UserListSerializer(users, many=True)
+        return Response(serializer.data)
 
 
 class PendingUsersAPIView(APIView):
